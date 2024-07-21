ff472a2e6ea2cd8392447ce911ef719d411683d7
jamie-codez <cruiseomondi90@gmail.com>
2024-07-21 07:53:40 +0300

    Finished the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import {Button} from "@/components/ui/button";
 import {DownloadIcon} from "lucide-react";
 import Socials from "@/components/Socials";
 import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 const Home = () => {
     return (
@@ -34,6 +35,7 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            <Stats/>
         </section>
     );
 }
---
+++ src/components/Stats.tsx
@@ -0,0 +1,63 @@
+"use client";
+import React from 'react';
+import CountUp from "react-countup";
+import {BriefcaseBusiness, Building, Coffee, CoffeeIcon, FolderKanban, GitCommit, PanelsTopLeft, Users, Workflow} from "lucide-react";
+import {Card} from "@/components/ui/card";
+
+const stats = [
+    {
+        num: 5,
+        text: "Years of experience",
+        icon: <BriefcaseBusiness/>
+    },
+    {
+        num: 26,
+        text: "Projects completed",
+        icon: <FolderKanban/>
+    },
+    {
+        num: 10,
+        text: "Technologies mastered",
+        icon: <PanelsTopLeft/>
+    },
+    {
+        num: 5000,
+        text: "Code commits",
+        icon: <GitCommit/>
+    },
+    {
+        num: 10,
+        text: "Happy clients",
+        icon: <Users/>
+    },
+    {
+        num: 5,
+        text: "Companies worked with",
+        icon: <Building/>
+    },
+    {
+        num: 100,
+        text: "Cups of coffee",
+        icon: <Coffee/>
+    }
+]
+
+
+const Stats = () => {
+    return (
+        <section className={"pt-4 pb-12 xl:pt-0 xl:pb-0"}>
+            <div className={"container mx-auto"}>
+                <div className={"flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none"}>
+                    {stats.map((stat, index) => (
+                        <div className={"flex-1 flex gap-4 items-center justify-center xl:justify-start"} key={index}>
+                            <CountUp end={stat.num} duration={5} delay={2} className={"text-4xl xl:text-6xl font-extrabold"}/>
+                            <p className={`${stat.text.length <15 ?"max-w-[100px]":"max-w-[150px]"} text-white/80 leading-snug`}>{stat.text}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Stats;
